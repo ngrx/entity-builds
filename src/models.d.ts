0257cd5beebe4df8f909a84f60ef34837f5c79cd
Brandon <robertsbt@gmail.com>
2018-10-22 18:43:21 +0000

    68390de refactor(store-devtools): rename recompute action type (#1375)

--- src/models.d.ts
+++ src/models.d.ts
@@ -1,31 +1,23 @@
-export declare type ComparerStr<T> = {
-    (a: T, b: T): string;
-};
-export declare type ComparerNum<T> = {
-    (a: T, b: T): number;
-};
+export declare type ComparerStr<T> = (a: T, b: T) => string;
+export declare type ComparerNum<T> = (a: T, b: T) => number;
 export declare type Comparer<T> = ComparerNum<T> | ComparerStr<T>;
-export declare type IdSelectorStr<T> = {
-    (model: T): string;
-};
-export declare type IdSelectorNum<T> = {
-    (model: T): number;
-};
+export declare type IdSelectorStr<T> = (model: T) => string;
+export declare type IdSelectorNum<T> = (model: T) => number;
 export declare type IdSelector<T> = IdSelectorStr<T> | IdSelectorNum<T>;
-export declare type DictionaryNum<T> = {
+export interface DictionaryNum<T> {
     [id: number]: T;
-};
+}
 export declare abstract class Dictionary<T> implements DictionaryNum<T> {
     [id: string]: T;
 }
-export declare type UpdateStr<T> = {
+export interface UpdateStr<T> {
     id: string;
     changes: Partial<T>;
-};
-export declare type UpdateNum<T> = {
+}
+export interface UpdateNum<T> {
     id: number;
     changes: Partial<T>;
-};
+}
 export declare type Update<T> = UpdateStr<T> | UpdateNum<T>;
 export interface EntityState<T> {
     ids: string[] | number[];
@@ -49,12 +41,12 @@ export interface EntityStateAdapter<T> {
     upsertOne<S extends EntityState<T>>(entity: T, state: S): S;
     upsertMany<S extends EntityState<T>>(entities: T[], state: S): S;
 }
-export declare type EntitySelectors<T, V> = {
+export interface EntitySelectors<T, V> {
     selectIds: (state: V) => string[] | number[];
     selectEntities: (state: V) => Dictionary<T>;
     selectAll: (state: V) => T[];
     selectTotal: (state: V) => number;
-};
+}
 export interface EntityAdapter<T> extends EntityStateAdapter<T> {
     selectId: IdSelector<T>;
     sortComparer: false | Comparer<T>;
