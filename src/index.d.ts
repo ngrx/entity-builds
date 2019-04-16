11dc352f1a4b348cde0dcba86eb5d4d51f8b23a3
GitHub <noreply@github.com>
2019-04-16 15:18:10 +0000

    dbfdbaf feat: introduce @ngrx/data library to the platform (#1754)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,2 @@
 export { createEntityAdapter } from './create_adapter';
-export { Dictionary, EntityState, EntityAdapter, Update, EntityMap, Predicate, } from './models';
+export { Dictionary, EntityState, EntityAdapter, Update, EntityMap, Predicate, IdSelector, Comparer, } from './models';
