6a506cdc9bbc406c1b6fe40217625a41ed66bba8
GitHub <noreply@github.com>
2020-06-02 14:17:44 +0000

    d7958e7 build: fix path resolution with library dependencies (#2554)

--- esm5/ngrx-entity.js
+++ esm5/ngrx-entity.js
@@ -7,4 +7,4 @@
  * Generated bundle index. Do not edit.
  */
 export { createEntityAdapter, Dictionary } from './index';
-//# sourceMappingURL=ngrx-entity.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmdyeC1lbnRpdHkuanMiLCJzb3VyY2VSb290Ijoibmc6Ly9AbmdyeC9lbnRpdHkvIiwic291cmNlcyI6WyJuZ3J4LWVudGl0eS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUlBLGdEQUFjLFNBQVMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJhdGVkIGJ1bmRsZSBpbmRleC4gRG8gbm90IGVkaXQuXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9pbmRleCc7XG4iXX0=
